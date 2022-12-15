025a178187f78d1088ddfb356af8f8402e58c1f9
Emily Menezes <estudosmenezes@gmail.com>
2022-12-15 17:49:09 -0300

    :bug: add 18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,9 +1,9 @@
 db.produtos.updateMany(
-    { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
-    { $push: { ingredientes: "bacon" } },
-  );
-  
-  db.produtos.find(
-    {},
-    { _id: false, nome: true, ingredientes: true },
-  );
+  { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+  { $addToSet: { ingredientes: "bacon" } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, ingredientes: 1, _id: 0 },
+);
